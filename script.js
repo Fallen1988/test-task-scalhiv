490e0b46c6e5b7bc10d0831d5a3bdd84b60f3a1b
Fallen1988 <orest.msp@gmail.com>
2018-02-20 14:42:15 +0200

    task 3. Rotate tile, rotate speed

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function randomNumber() {
 }
 
 function addDotes() {
+    tile.style.transform = `rotate(0deg) scale(0.7)`;
+    speedInput.value = 0.1;
+    tileSize.value = 1;
+    tile.style.transition = speedInput.value + 's';
+
     const topHalf = document.querySelector('.top');
     const bottomHalf = document.querySelector('.bottom');
 
@@ -14,18 +19,42 @@ function addDotes() {
 
     topHalf.dataset.dots =  String(topValue);
     bottomHalf.dataset.dots = String(bottomValue);
-    console.log(topHalf.dataset.dots)
 }
 
 
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', addDotes);
 
+let transform = {
+    rotate: 0,
+    scale: 0.7
+};
+let degrees = 0;
+function rotateTile() {
+    //deg = ;
+    degrees += +this.dataset.rotate;
 
+    transform.rotate = degrees;
+    console.log(transform);
+    tile.style.transform = `rotate(${transform.rotate}deg) scale(${transform.scale})`;
+}
 
+const tile = document.querySelector('.tile');
+const rotateButtons = document.querySelectorAll('.btn[data-rotate]');
+rotateButtons.forEach(button => button.addEventListener('click', rotateTile));
 
+function changeSpeed(e) {
+    tile.style.transition = 1/e.target.value + 's';
+}
+const speedInput = document.querySelector('.rotation-speed');
 
+speedInput.addEventListener('change', changeSpeed);
+//speedInput.addEventListener('mousemove', changeSpeed);
 
-
-/*const reset = document.querySelector('.reset');
-reset.addEventListener('click', () => {
-
-});*/
+function changeSize(e) {
+    transform.scale = e.target.value;
+    tile.style.transform = `rotate(${transform.rotate}deg) scale(${transform.scale})`;
+}
+const tileSize = document.querySelector('.tile-size');
+tileSize.addEventListener('change', changeSize);
+//tileSize.addEventListener('mousemove', changeSize);
