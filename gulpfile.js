619b354e30937ea41604d0e3b35a26e0aac72379
Fallen1988 <orest.msp@gmail.com>
2018-02-20 20:56:03 +0200

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,11 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const browserSync = require('browser-sync').create();
-//const minify = require('gulp-minifier');
-//var minify = require("gulp-csso");
-//var rename = require("gulp-rename");
 
 gulp.task("sass", function () {
-    gulp.src("sass/style.scss")      //звідки ми все починаємо
+    gulp.src("sass/style.scss")
         .pipe(sass())
-        .pipe(gulp.dest("css"))     // в яку папку зберегти результат
-        // .pipe(minify())
-        // .pipe(rename("style.min.css"))
-        // .pipe(gulp.dest("build/css"))
+        .pipe(gulp.dest("css"))
         .pipe(browserSync.reload({
             stream: true
         }));
@@ -20,7 +14,6 @@ gulp.task("sass", function () {
 gulp.task('watch', ['sass' ,'browserSync'], function () {
     gulp.watch('sass/**/*.scss', ['sass']);
     gulp.watch("*.html").on("change", browserSync.reload);
-    // Other watchers
 });
 
 gulp.task('browserSync', function () {
@@ -30,22 +23,3 @@ gulp.task('browserSync', function () {
         }
     })
 });
-
-/*
-gulp.task('example', function() {
-    return gulp.src('example/src/!**!/!*').pipe(minify({
-        minify: true,
-        minifyHTML: {
-            collapseWhitespace: true,
-            conservativeCollapse: true,
-        },
-        minifyJS: {
-            sourceMap: true
-        },
-        minifyCSS: true,
-        getKeptComment: function (content, filePath) {
-            let m = content.match(/\/\*![\s\S]*?\*\//img);
-            return m && m.join('\n') + '\n' || '';
-        }
-    })).pipe(gulp.dest('example/dest'));
-});*/
